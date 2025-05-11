e968f54c381a2101f681d63cfb6243d11890de01
Azuritberg <143940692+Azuritberg@users.noreply.github.com>
2025-05-11 20:26:07 +0200

    asd

--- index.js
+++ index.js
@@ -2,3 +2,29 @@
 import { Gigs, DJs, Producers, Cities } from './data.js';
 
 // Skapa alla Grafer här!!
+
+
+// Tillfälliga knappar OBS!! vi får ändra detta sedan så vi kan generera alla knappar
+export const cities = ["Agrabah", "Sunnydale", "Rimini", "Karatas", "Asteroid City"]; // importera från data.js
+export const Producers = ["Trance AB", "No Mind AB", "Festen AB", "Gigskaparna", "Xtas Produktioner"];
+
+
+const cityContainer = d3.select(".city-buttons");
+
+cityContainer.selectAll("button")
+  .data(Cities)
+  .enter()
+  .append("button")
+  .attr("class", "btn-city")
+  .text(d => d);
+
+
+
+const producerContainer = d3.select(".producer-buttons");
+
+producerContainer.selectAll("button")
+  .data(Producers)
+  .enter()
+  .append("button")
+  .attr("class", "btn-producer")
+  .text(d => d);
