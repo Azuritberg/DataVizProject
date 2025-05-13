a1a4c434824bc2dd0112ff3f15388d7a9e0c165a
Ronja <143420962+rvrdotter@users.noreply.github.com>
2025-05-13 11:53:18 +0200

    added dataset generation to main
    dataset generation from other branch has been integrated, the main forms of datasets should now be available to be visualized!

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-//import { Gigs, DJs, Producers, Cities } from './data.js';
+import { Gigs, DJs, Producers, Cities } from './data.js';
 
 // Skapa alla Grafer här!!
 
@@ -29,7 +29,162 @@ producerContainer.selectAll("button")
   .attr("class", "btn-producer")
   .text(d => d);
 */
+let dataSet = []
+let dataSet2 = []
+let dataSetProducersEth = [];
+let dataSetProducersGen = [];
 
+let genders = ["lambda", "theta", "omicron"];
+let ethinicties = ["psi", "rho", "tau"];
+
+let dataSetAvgEarningsGender = [];
+let dataSetAvgEarningsEthnicity = [];
+
+let dataSetTotalGigsGender = [];
+let dataSetTotalGigsEthnicity = [];
+
+//creates dataset where each city is shown by it's gig count broken down in terms of each DJs gender and ethinicity
+for (const city of Cities) {
+    let cityGigs = Gigs.filter(x => x.cityID == city.id);
+    let dataPoint = {
+        name: city.name,
+        id: city.id,
+        data: []
+    }
+    let dataPoint2 = {
+        name: city.name,
+        id: city.id,
+        data: []
+    }
+    for(let i = 0; i < 10; i++){
+        let yearsGigs = cityGigs.filter(x => {
+            let year = new Date(x.date).getFullYear();
+            return year == 2015 + i;
+        });
+        let yearPoint = {
+            year: 2015 + i,
+            rho:0,
+            tau:0,
+            psi:0,
+        };
+        let yearPointGender = {
+            year: 2015 + i,
+            lambda: 0,
+            omicron: 0,
+            theta: 0,
+        }
+        let djIDs = yearsGigs.map(x => {return x.djID})
+        let eths = djIDs.map(x => {
+            return DJs.find(y => y.id == x).ethnicity;
+        })
+        let genders = djIDs.map(x => {
+            return DJs.find(y => y.id == x).gender;
+        })
+        yearPointGender.theta = genders.filter(x => x == "theta").length;
+        yearPointGender.omicron = genders.filter(x => x == "omicron").length;
+        yearPointGender.lambda = genders.filter(x => x == "lambda").length;
+        dataPoint2.data.push(yearPointGender);
+        yearPoint.rho = eths.filter(x => x == "rho").length;
+        yearPoint.psi = eths.filter(x => x == "psi").length;
+        yearPoint.tau = eths.filter(x => x == "tau").length;
+        dataPoint.data.push(yearPoint);
+    }
+    dataSet.push(dataPoint);
+    dataSet2.push(dataPoint2);
+}
+//same as above but for producers not cities
+for (const producer of Producers) {
+    console.log(producer.id)
+    let producerGigs = Gigs.filter(x => {return x.producerID == producer.id});
+    let dataPointEth = {
+        name: producer.name,
+        id: producer.id,
+        data: []
+    }
+    let dataPointGen = {
+        name: producer.name,
+        id: producer.id,
+        data: []
+    }
+    for(let i = 0; i < 10; i++){
+        let yearsGigs = producerGigs.filter(x => {
+            let year = new Date(x.date).getFullYear();
+            return year == 2015 + i;
+        });
+        let yearPointEth = {
+            year: 2015 + i,
+            rho:0,
+            tau:0,
+            psi:0,
+        };
+        let yearPointGen = {
+            year: 2015 + i,
+            lambda: 0,
+            omicron: 0,
+            theta: 0,
+        }
+        let djIDs = yearsGigs.map(x => {return x.djID})
+        let eths = djIDs.map(x => {
+            return DJs.find(y => y.id == x).ethnicity;
+        })
+        let genders = djIDs.map(x => {
+            return DJs.find(y => y.id == x).gender;
+        })
+        yearPointGen.theta = genders.filter(x => x == "theta").length;
+        yearPointGen.omicron = genders.filter(x => x == "omicron").length;
+        yearPointGen.lambda = genders.filter(x => x == "lambda").length;
+        dataPointGen.data.push(yearPointGen);
+        yearPointEth.rho = eths.filter(x => x == "rho").length;
+        yearPointEth.psi = eths.filter(x => x == "psi").length;
+        yearPointEth.tau = eths.filter(x => x == "tau").length;
+        dataPointEth.data.push(yearPointEth);
+    }
+    dataSetProducersEth.push(dataPointEth);
+    dataSetProducersGen.push(dataPointGen);
+}
+console.log(dataSetProducersEth, dataSetProducersGen);
+console.log(dataSet, dataSet2)
+
+
+//skapar ett en array filtered gigs som är alla gigsen för ett kön
+for (const gender of genders) {
+    //filter gigs by gender
+    let filteredGigs = Gigs.filter(x => {
+        let gigDj = x.djID;
+        let DJgender = DJs.find(y => y.id == gigDj).gender;
+        return DJgender == gender;
+    });
+    let gigEarnings = filteredGigs.map(x => x.djEarnings).reduce((cv, pv) => {return cv + pv}, 0) / filteredGigs.length; 
+    let genderedGigs = filteredGigs.length;
+    dataSetAvgEarningsGender.push({
+        gender: gender,
+        earnings: gigEarnings
+    });
+    dataSetTotalGigsGender.push({
+        gender: gender,
+        totalGigs : genderedGigs
+    });
+}
+for (const ethinicity of ethinicties) {
+    let filteredGigs = Gigs.filter(x => {
+        let gigDj = x.djID;
+        let DJethnicity = DJs.find(y => y.id == gigDj).ethnicity;
+        return DJethnicity == ethinicity;
+    });
+    let gigEarnings = filteredGigs.map(x => x.djEarnings).reduce((cv, pv) => {return cv + pv}, 0) / filteredGigs.length;
+    let genderedGigs = filteredGigs.length;
+    dataSetAvgEarningsEthnicity.push({
+        ethinicity: ethinicity,
+        earnings: gigEarnings
+    });
+    dataSetTotalGigsEthnicity.push({
+        ethnicity: ethinicity,
+        totalGigs: genderedGigs
+    });
+}
+
+console.log(dataSetAvgEarningsGender, dataSetAvgEarningsEthnicity);
+console.log(dataSetTotalGigsGender, dataSetTotalGigsEthnicity);
 
 
 // Tillfälliga knappar OBS!! vi får ändra detta sedan så vi kan generera alla knappar
